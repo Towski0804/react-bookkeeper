8ced67e1932d5661598a017e5d7618e7f5beb3ec
Towski0804 <superchao0206@gmail.com>
2024-04-03 16:35:48 +0800

    Change deploy to remote script.

--- bin/deploy_to_remote.sh
+++ bin/deploy_to_remote.sh
@@ -4,9 +4,9 @@ pnpm run build
 
 tar -czvf dist.tar.gz dist 
 
-scp ./dist.tar.gz gddEC2:/home/ubuntu/bookkeeper &&
+scp ./dist.tar.gz gddUSW:/home/ubuntu/bookkeeper &&
 rm -rf dist.tar.gz &&
 
-ssh gddEC2 sudo rm -rf /home/ubuntu/bookkeeper/dist &&
-ssh gddEC2 tar -xzvf /home/ubuntu/bookkeeper/dist.tar.gz -C /home/ubuntu/bookkeeper && 
-ssh gddEC2 sudo rm -rf /home/ubuntu/bookkeeper/dist.tar.gz
+ssh gddUSW sudo rm -rf /home/ubuntu/bookkeeper/dist &&
+ssh gddUSW tar -xzvf /home/ubuntu/bookkeeper/dist.tar.gz -C /home/ubuntu/bookkeeper && 
+ssh gddUSW sudo rm -rf /home/ubuntu/bookkeeper/dist.tar.gz
